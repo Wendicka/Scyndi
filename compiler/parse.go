e17d0df8f18e0488ec6e5563b77fa31512b82d93
Tricky1975 <facebook@tbbs.nl>
2018-06-27 21:29:52 +0200

    Code grab separated from pre-separator

--- compiler/parse.go
+++ compiler/parse.go
@@ -2,15 +2,33 @@ package scynt
 
 import (
 		"trickyunits/qff"
+jcr		"trickyunits/jcr6/jcr6main"
 )
 
-// Basically all this routine does is make sure all instructions are properly separated.
-// New lines (0x0A) and ; can do this, although those who prefer it can turn off the new-line as separation.
-func pre_parse(sourcefile string) *tsource{
-	doing("Analysing: ",sourcefile)
+func grabfromfile(sourcefile string) *[]byte{
+	doing("Reading file: ",sourcefile)
 	bank,e:=qff.EGetFile(sourcefile)
 	if e!=nil { ethrow(e) }
 	if bank[0]=='"' { throw("Not a single file in Scyndi may start with a \"!") }
-	ret:= &tsource{} 
+	done()
+	return &bank
+}
+
+func grabfromjcr(j jcr.TJCRDir,entry string) *[]byte{
+	doing("Reading JCR entry: ",sourcefile)
+	bank:=jcr.JCR_B(j,entry)
+	if jcr.JCRERROR!="" { throw(jcr.JCRERROR) }
+	if bank[0]=='"' { throw("Not a single file in Scyndi may start with a \"!") }
+	done()
+	return &bank
+}
+
+
+// Basically all this routine does is make sure all instructions are properly separated.
+// New lines (0x0A) and ; can do this, although those who prefer it can turn off the new-line as separation.
+func sepsource(*[] byte,file string) *tsource {
+	ret:=&tsource{}
+	doing("Pre-parse analysing: ",file)
+	ret.nlsep = NLSEP
 	return ret
 }
