91ec562ee611c531ce13326027129b0e5e90c7bc
Tricky1975 <facebook@tbbs.nl>
2018-07-14 12:35:08 +0200

    fixes

--- compiler/okki.pas
+++ compiler/okki.pas
@@ -10,30 +10,31 @@ var
 	
 begin
 	writeln('package scynt');
-	writeln('var okki = map[sring]string{}');
+	writeln('var okki = map[string]byte{}');
 	writeln('func init(){');
 	dl[1]:='r';
 	dl[2]:='b';
 	dl[3]:='n';
-	dl[4]:='0';
-	for i:=1 to 4 do 
-		writeln('	okki["\\',dl[i],'"]=''\\',dl[i],''';');
+	{dl[4]:='0';}
+	for i:=1 to 3 do 
+		writeln('	okki["\\',dl[i],'"]=''\',dl[i],''';');
 	for i:=1 to 10 do
 		digi[i]:=0;
-	for i:=1 to 255 do begin
+	digi[1]:=-1;
+	for i:=0 to 255 do begin
 		digi[1]:=digi[1]+1;
 		for j:=1 to 9 do begin
 			if digi[j]>7 then begin
-				digi[j]:=digi[j]-7;
+				digi[j]:=digi[j]-8;
 				digi[j+1]:=digi[j+1]+1;
 				if aantal<j+1 then aantal:=j+1
 			end
 		end;
-		write('	okki["\0');
-		for j:=1 to aantal do write(digi[j]);
-		write('"] = ''\0');
-		for j:=1 to aantal do write(digi[j]);
-		writeln(''';	{',i,'}')
+		write('	okki["\');
+		for j:=3 downto 1 do write(digi[j]);
+		write('"] = ''\');
+		for j:=3 downto 1 do write(digi[j]);
+		writeln(''';	// ',i:3,' //')
 	end;
 	writeln('}')
 end.
