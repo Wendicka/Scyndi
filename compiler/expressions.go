4126f71e4cec492e4aec6b45e7fb5f9fc0ea2094
Tricky1975 <facebook@tbbs.nl>
2018-07-16 12:00:18 +0200

    ex-head code

---
+++ compiler/expressions.go
@@ -0,0 +1,8 @@
+package scynt
+
+func defaultexpressiontranslation(expect string,ol *tori,start,level int) (endpos int,ex string){
+}
+
+
+func (s *tsource) translateExpressions(expect string,ol *tori,start,level int) (endpos int,ex string){
+}
