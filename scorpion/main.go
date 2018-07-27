0371d6b5893a0ed79285fbcf338d2f6d563df48c
Tricky1975 <facebook@tbbs.nl>
2018-07-27 16:11:07 +0200

    Ansi control - closes #47

--- scorpion/main.go
+++ scorpion/main.go
@@ -20,7 +20,7 @@
 		
 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
 	to the project the exceptions are needed for.
-Version: 18.07.22
+Version: 18.07.27
 */
 package main
 
@@ -30,6 +30,7 @@ import ans "trickyunits/ansistring"
 import "trickyunits/mkl"
 import "os"
 import "fmt"
+import "strings"
 import "trickyunits/gini"
 import "trickyunits/dirry"
 
@@ -37,13 +38,18 @@ var sourcefile string
 var outputpath string
 
 func init(){
-mkl.Version("Scyndi Programming Language - main.go","18.07.22")
+mkl.Version("Scyndi Programming Language - main.go","18.07.27")
 mkl.Lic    ("Scyndi Programming Language - main.go","GNU General Public License 3")
 	nonl:=flag.Bool("sco",false,"If set new lines will not count as the end of an instruction and all instructions will have to be ended with a semi-colon")
 	trgt:=flag.String("target","Wendicka","Set the target to translate to. (Supported targets: "+Scyndi.TargetsSupported()+")")
 	outp:=flag.String("o","","Output file/dir. Please note that the output effect can be different depending on the chosen target")
 	ver :=flag.Bool("version",false,"Show version information of all used source files to build Scorpion")
+	ansi:=flag.String("ansi","","ON = force ANSI to be ON or OFF")
 	flag.Parse()
+	switch strings.ToUpper(*ansi) {
+		case "ON":	ans.ANSI_Use=true
+		case "OFF":	ans.ANSI_Use=false
+	}
 	if *ver {
 		Copyright()
 		fmt.Println(mkl.ListAll())
