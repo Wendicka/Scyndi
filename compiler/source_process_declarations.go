e34b462db1883164456d043e3645de180a51af24
Tricky1975 <facebook@tbbs.nl>
2019-02-08 15:53:21 +0100

    headpurecode

--- compiler/source_process_declarations.go
+++ compiler/source_process_declarations.go
@@ -1,27 +1,28 @@
-/*
-	Scyndi
-	Souce Progress Declarations
-	
-	
-	
-	(c) Jeroen P. Broks, 2018, All rights reserved
-	
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-		
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses/>.
-		
-	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
-	to the project the exceptions are needed for.
-Version: 18.08.11
-*/
+// License Information:
+// 	Scyndi
+// 	Souce Progress Declarations
+// 	
+// 	
+// 	
+// 	(c) Jeroen P. Broks, 2018, 2019, All rights reserved
+// 	
+// 		This program is free software: you can redistribute it and/or modify
+// 		it under the terms of the GNU General Public License as published by
+// 		the Free Software Foundation, either version 3 of the License, or
+// 		(at your option) any later version.
+// 		
+// 		This program is distributed in the hope that it will be useful,
+// 		but WITHOUT ANY WARRANTY; without even the implied warranty of
+// 		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// 		GNU General Public License for more details.
+// 		You should have received a copy of the GNU General Public License
+// 		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 		
+// 	Exceptions to the standard GNU license are available with Jeroen's written permission given prior 
+// 	to the project the exceptions are needed for.
+// Version: 19.02.08
+// End License Information
+
 package scynt
 
 import (
@@ -167,6 +168,7 @@ type tsource struct {
 	userequested []string
 	usepurecode string
 	allid map[string]*tidentifier // All own identifiers plus the imported ones.
+	headpurecode string
 }
 
 func (s *tsource) GetIdentifier(aname string,c *tchunk, o *tori) *tidentifier {
